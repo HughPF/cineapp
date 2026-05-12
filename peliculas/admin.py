05977ae30129486234b3d583aca56df6702fe932
Hugo Pérez Fermoso <hperezfermoso2@gmail.com>
2026-05-12 19:48:19 +0200

    Initial migration and admin registration

--- peliculas/admin.py
+++ peliculas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Pelicula, Genero
 
-# Register your models here.
+
+@admin.register(Genero)
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+    search_fields = ('nombre',)
+
+
+@admin.register(Pelicula)
+class PeliculaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'director', 'anio', 'genero', 'autor', 'fecha_creacion')
+    list_filter = ('genero', 'anio', 'autor')
+    search_fields = ('titulo', 'director', 'sinopsis')
+    date_hierarchy = 'fecha_creacion'
