3c165cafa89535d019273f3b60ebb6189e5423b0
Hugo Pérez Fermoso <hperezfermoso2@gmail.com>
2026-05-12 19:47:42 +0200

    Add Genero model and FK relation

--- peliculas/models.py
+++ peliculas/models.py
@@ -3,6 +3,19 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class Genero(models.Model):
+    nombre = models.CharField(max_length=50, unique=True, verbose_name='Nombre')
+    descripcion = models.CharField(max_length=200, blank=True, verbose_name='Descripción')
+
+    class Meta:
+        verbose_name = 'Género'
+        verbose_name_plural = 'Géneros'
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
+
+
 class Pelicula(models.Model):
     titulo = models.CharField(max_length=200, verbose_name='Título')
     director = models.CharField(max_length=150, verbose_name='Director/a')
@@ -10,6 +23,10 @@ class Pelicula(models.Model):
     anio = models.PositiveIntegerField(verbose_name='Año de estreno')
     duracion = models.PositiveIntegerField(
         verbose_name='Duración (minutos)', default=90
+    )
+    genero = models.ForeignKey(
+        Genero, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='peliculas', verbose_name='Género'
     )
     autor = models.ForeignKey(
         User, on_delete=models.CASCADE,
