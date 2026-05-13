c061061e2e658418ad94d715666096f3598ea094
Hugo Pérez Fermoso <hperezfermoso2@gmail.com>
2026-05-13 20:53:33 +0200

    Configure URL routing for peliculas app

--- cineapp/urls.py
+++ cineapp/urls.py
@@ -2,8 +2,9 @@
 URL configuration for cineapp project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('peliculas.urls')),
 ]
